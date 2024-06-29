cfc16d1f45922e1d2c44b857aac5f0faa19980a5
Luan Almeida <luan.almeida@jbtec.com.br>
2024-06-29 17:42:25 -0300

    feat: implement multiple routes

--- src/features/users/controller/core.rs
+++ src/features/users/controller/core.rs
@@ -3,8 +3,14 @@ extern crate rocket;
 use rocket::serde::json::{Value, json};
 use crate::features::users::model::db::find_users;
 
-#[get("/")]
+#[get("/users")]
 pub fn list_users() -> Value {
     let result = find_users();
     return json!({"users": result});
+}
+
+#[get("/users/<_id>")]
+pub fn list_users_by_id(_id: i32) -> Value {
+    let result = find_users();
+    return json!({"users": {"result": result, "id": _id }});
 }
--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,15 @@ mod features;
 
 #[macro_use] extern crate rocket;
 
+use features::users::controller::core::{list_users,list_users_by_id};
+
 #[rocket::main]
 async fn main() {
     let _ = rocket::build()
-        .mount("/", routes![features::users::controller::core::list_users])
+        .mount("/", routes![
+            list_users,
+            list_users_by_id
+        ])
         .launch()
         .await;
 }
